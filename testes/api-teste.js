b661f7eb7ccaf0729aa6075f5af0069e10622903
thassiov <tvmcarvalho@gmail.com>
2017-04-27 11:40:17 -0300

    Adicionando arquivo de testes da API

---
+++ testes/api-teste.js
@@ -0,0 +1,13 @@
+/**
+    * @desc Teste da API
+    */
+
+'use strict';
+
+const teste = require('ava');
+//const server = require('../index.js');
+
+
+teste('teste de endpoint | POST /', t => {
+    t.pass();
+});
