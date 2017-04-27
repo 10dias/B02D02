b97c955f8c80f37b253877c3e947bfdd32618fb4
thassiov <tvmcarvalho@gmail.com>
2017-04-27 11:51:56 -0300

    TESTE: adicionando teste de hashing

--- testes/api-teste.js
+++ testes/api-teste.js
@@ -5,9 +5,20 @@
 'use strict';
 
 const teste = require('ava');
-//const server = require('../index.js');
+const server = require('../index.js');
 
 
 teste('teste de endpoint | POST /', t => {
-    t.pass();
+    const req = {
+        method: 'POST',
+        url: '/',
+        payload: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non ultricies diam. Aenean ac molestie diam. Vestibulum arcu turpis, placerat. '
+    };
+
+    return server.inject(req)
+        .then(res => {
+            const d = JSON.parse(res.payload);
+            console.log('MD5: ' + d);
+            t.is(typeof(d.hash).toString(), 'string');
+        });
 });
